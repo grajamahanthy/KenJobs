8be77def4aa4ac75728b9a84e2242ae9073258c4
Ganesh Rajamahanthy <g.rajamahanthy@gmail.com>
2019-05-09 02:06:28 +0530

    Auth and Common Framework Changes

---
+++ API/KenJobs.Api/KenJobs.Dal/Workers/GenericRepository.cs
@@ -0,0 +1,88 @@
+﻿using KenJobs.Dal.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KenJobs.Dal.Workers
+{
+    class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private KenJobsEntities _context = null;
+        private DbSet<T> table = null;
+        public GenericRepository()
+        {
+            this._context = new KenJobsEntities();
+            table = _context.Set<T>();
+        }
+        public GenericRepository(KenJobsEntities _context)
+        {
+            this._context = _context;
+            table = _context.Set<T>();
+        }
+        public IEnumerable<T> GetAll()
+        {
+            return table.ToList();
+        }
+        public T GetById(object id)
+        {
+            return table.Find(id);
+        }
+        public void Insert(T obj)
+        {
+            table.Add(obj);
+        }
+        public void Update(T obj)
+        {
+            table.Attach(obj);
+            _context.Entry(obj).State = EntityState.Modified;
+        }
+        public void Delete(object id)
+        {
+            T existing = table.Find(id);
+            table.Remove(existing);
+        }
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+
+
+        #region Implementing IDiosposable
+
+        #region private dispose variable declaration
+        private bool disposed = false;
+        #endregion
+
+        /// <summary>
+        /// Protected Virtual Dispose method
+        /// </summary>
+        /// <param name="disposing"></param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    //Debug.WriteLine("UnitOfWork is being disposed");
+                    _context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        /// <summary>
+        /// Dispose method
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        #endregion
+    }
+}
